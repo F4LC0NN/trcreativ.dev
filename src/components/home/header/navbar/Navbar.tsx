811b831020ff60703cd52396b33ac84a7b3ae778
F4LC0NN <thomasruben25@gmail.com>
2022-06-17 15:54:27 +0200

    remove useCallback import

--- src/components/home/header/navbar/Navbar.tsx
+++ src/components/home/header/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { motion, useCycle } from 'framer-motion';
 
 import logoBlue from '../../../../assets/images/header/logoBlueIcon.svg';
